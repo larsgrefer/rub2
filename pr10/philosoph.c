2275cd551a1c0099698bc0e61f6893437a01195c
brunothg <android.bruns@googlemail.com>
2014-06-01 18:15:32 +0200

    Praktikum 10 ----------------- Die ganze Aufgabenstellung passte mal wieder nicht zum vorgegebenen Quelltext. Kp ob das so gemeint war. Hab aber auch keine Lust zu raten, was die wollen. Viel Spaß beim grübeln :)

--- pr10/philosoph.c
+++ pr10/philosoph.c
@@ -1,13 +1,19 @@
 #include <pthread.h>
 #include <stdio.h>
-#define NR_OF_PHIL 5
+#include <stdlib.h>
 
-static int gabel_vorhanden[NR_OF_PHIL],rechts[NR_OF_PHIL],links[NR_OF_PHIL]; 
-                mutex=                ;
+#define NR_OF_PHIL 5
+#define NR_OF_ROUNDS 5
+
+static int gabel_vorhanden[NR_OF_PHIL], rechts[NR_OF_PHIL], links[NR_OF_PHIL];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *phil(void *arg);
+int gabeln_holen(int);
+void gabeln_abgeben(int);
+void init_gabeln(int);
 
-int main (void)
+int main(void)
 {
 	int i,status;
 	pthread_t thread[NR_OF_PHIL];
@@ -15,63 +21,93 @@ int main (void)
 	init_gabeln(NR_OF_PHIL);
 
 	/* Starte die Threads */
-	for(i=0;i<NR_OF_PHIL;i++){
-		status=              (                     );
-		if(status!=0){
-			fprintf(stderr,"Error: Konnte Thread %d nicht erzeugen\n",i);
+	for(i = 0; i < NR_OF_PHIL; i++){
+		int *nr = malloc(sizeof(int));
+		*nr = i;
+
+		int status = pthread_create(&thread[i], NULL, phil, (void*)nr);
+
+		if(status != 0){
+			fprintf(stderr, "Error: Konnte Thread %d nicht erzeugen\n", i);
 			exit(-1);		
 		}
 	}
 
 	/* Warte auf Terminierung */
-	for(i=0;i<NR_OF_PHIL;i++)
-		              (thread[i],NULL);
+	for(i = 0; i < NR_OF_PHIL; i++){
+		pthread_join(thread[i], NULL);
+	}
 
 	exit(0);
 }
 
 void *phil(void *arg){
 
-	int nr = (int)arg;
+	int *argi = (int*)arg;
+	int nr = *argi;
+
 	int i;
 
-	printf("Philosoph %d gestartet\n",nr);
+	printf("Philosoph %d gestartet\n", nr);
 
-	for(i=0;i<20;i++)
+	for(i = 0; i < NR_OF_ROUNDS; i++)
 	{
-		 
-		printf("Philosoph %d isst\n",nr);
-		sleep(nr/2);
-		 
+		gabeln_holen(nr);
+		printf("Philosoph %d isst\n", nr);
+
+		sleep(nr / 2);
+
+		gabeln_abgeben(nr);
+
 		sleep(1);
 	}
 
-	printf("Philosoph %d ist fertig\n",nr);
+	printf("Philosoph %d ist fertig\n", nr);
 
+	free(argi);
 	return NULL;
 }
 
-int gabeln_holen(int i){
+int gabeln_holen(int nr){
+	int erfolg = 0;
 
-	int erfolg=0;
+	while(!erfolg){
+		pthread_mutex_lock(&mutex);
 
-	 
-	
+		if(gabel_vorhanden[nr] == 2){
+
+			gabel_vorhanden[nr] = 0;
+			gabel_vorhanden[links[nr]] -= 1;
+			gabel_vorhanden[rechts[nr]] -= 1;
+
+			erfolg = 1;
+		}
+
+		pthread_mutex_unlock(&mutex);
+	}
+
 	return erfolg;
 }
 
-void gabeln_abgeben(int i){
+void gabeln_abgeben(int nr){
 
- 
+	pthread_mutex_lock(&mutex);
+
+		gabel_vorhanden[nr] = 2;
+
+		gabel_vorhanden[links[nr]] += 1;
+		gabel_vorhanden[rechts[nr]] += 1;
+
+	pthread_mutex_unlock(&mutex);
 }
 
 void init_gabeln(int nr){
 
 	int i;
 
-	for(i=0;i<nr;i++){
-		gabel_vorhanden[i]=2;
-		rechts[i]=(i+1)%nr;
-		links[i]=(i-1+nr)%nr;
+	for(i = 0; i < nr; i++){
+		gabel_vorhanden[i] = 2;
+		rechts[i] = (i + 1) % nr;
+		links[i] = (i - 1 + nr) % nr;
 	}
 }
