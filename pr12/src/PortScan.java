be69915e42dbcb756254b3a535e390a82c712f06
Dennis Fricke <dennis.fricke@gmx.com>
2014-06-18 13:18:38 +0200

    reformat

--- pr12/src/PortScan.java
+++ pr12/src/PortScan.java
@@ -1,24 +1,23 @@
 /** Portscanner sequentiell
-*  Prof. Dr. Rolf Swik
-*/
-import java.net.*;
+ *  Prof. Dr. Rolf Swik
+ */
+
 import java.io.IOException;
+import java.net.Socket;
 
 public class PortScan {
 
-  public static void main(String[] args) {
-      String host = args[0];
-	int pa =java.lang.Integer.parseInt(args[1]);
- 	int pe =java.lang.Integer.parseInt(args[2]);
-           for (int port = pa; port <= pe; port++) {
-      		try {
-        	Socket s = new Socket(host, port); 
-        	System.out.println(host + " hoert auf Port " + port);
-        	s.close();
-      		}
-      		catch (IOException e) {}
+	public static void main(String[] args) {
+		String host = args[0];
+		int pa = java.lang.Integer.parseInt(args[1]);
+		int pe = java.lang.Integer.parseInt(args[2]);
+		for (int port = pa; port <= pe; port++) {
+			try {
+				Socket s = new Socket(host, port);
+				System.out.println(host + " hoert auf Port " + port);
+				s.close();
+			} catch (IOException e) {
+			}
+		}
 	}
-   }
 }
-
-
--- pr12/src/com/fh/rub2/pr12/Main/Main.java
+++
@@ -1,8 +0,0 @@
-package com.fh.rub2.pr12.Main;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
